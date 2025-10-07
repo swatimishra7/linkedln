70eae241f2f0337d30f8f2f60a3961fb008c9880
swatimishra7 <123swati2014@sjcem.edu.in>
2025-10-07 10:24:47 +0530

    chnages

---
+++ llm_helper.py
@@ -0,0 +1,15 @@
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="openai/gpt-oss-20b")
+
+
+if __name__ == "__main__":
+    response = llm.invoke("Two most important ingradient in samosa are ")
+    print(response.content)
+
+
+
+

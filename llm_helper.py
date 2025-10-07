4d09b3a3b226459229cd6fddbd9eb0bb8ee26b66
swatimishra7 <123swati2014@sjcem.edu.in>
2025-10-07 11:45:03 +0530

    chnages

--- llm_helper.py
+++ llm_helper.py
@@ -1,8 +1,13 @@
-from langchain_groq import ChatGroq
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+# Load local .env if available (for local development)
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ModuleNotFoundError:
+    pass  # Streamlit Cloud will use secrets
+
+from langchain_groq import ChatGroq
 llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="openai/gpt-oss-20b")
 
 
